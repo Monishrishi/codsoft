319e12630ebab451914a8dd96bbaeee96a46993d
Monishrishi <160614172+Monishrishi@users.noreply.github.com>
2024-08-02 12:30:34 +0530

    Add files via upload

---
+++ codsoft t-3/cal.js
@@ -0,0 +1,14 @@
+
+let d=document.getElementById("displaybox");
+function display(val)
+{
+    d.value+=val;
+}
+function clearl()
+{
+    d.value=" ";
+}
+function calcu()
+{
+    d.value=eval(d.value);
+}
